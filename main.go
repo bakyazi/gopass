63ca3b474fadd20642c41b4e8bc64ca4e575979a
Berkay Akyazi <berkay.akyazi@picussecurity.com>
2022-10-15 22:01:11 +0300

    main

--- main.go
+++ main.go
@@ -1,18 +1,7 @@
 package main
 
-import (
-	"github.com/bakyazi/gopass/sheetsapi"
-)
+import "github.com/bakyazi/gopass/cmd"
 
 func main() {
-	db, err := sheetsapi.NewPasswordDB("creds.json", "1u8W_mNDPl8xRsl2YPrqk46I2oOZ_NDnZUpFThRGMo5A")
-	if err != nil {
-		panic(err)
-	}
-
-	err = db.DeletePassword("twitter.com", "berkay15")
-	if err != nil {
-		panic(err)
-	}
-
+	cmd.Execute()
 }
