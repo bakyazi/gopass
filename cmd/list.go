43f830340fc9bfe9a296322cff1f288f6ad123a2
Berkay Akyazi <berkay.akyazi@picussecurity.com>
2022-10-15 22:01:45 +0300

    list command

---
+++ cmd/list.go
@@ -0,0 +1,41 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/bakyazi/gopass/config"
+	"github.com/bakyazi/gopass/sheetsapi"
+	"github.com/spf13/cobra"
+	"os"
+	"text/tabwriter"
+)
+
+var listCmd = &cobra.Command{
+	Use: "list",
+	Run: func(cmd *cobra.Command, args []string) {
+		cfg := config.GetConfig()
+
+		db, err := sheetsapi.NewPasswordDB(cfg.CredentialFile, cfg.SheetId)
+		if err != nil {
+			panic(err)
+		}
+		passw, err := db.GetPasswords(1, 5)
+		if err != nil {
+			fmt.Println("Failed to list all passwords", err.Error())
+			return
+		}
+		w := new(tabwriter.Writer)
+		w.Init(os.Stdout, 8, 8, 2, '\t', 0)
+
+		defer w.Flush()
+		fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", "Row", "Site", "Username", "Password")
+		fmt.Fprintf(w, "\n %s\t%s\t%s\t%s\t", "----", "----", "----", "----")
+		for _, p := range passw {
+			fmt.Fprintf(w, "\n %d\t%s\t%s\t%s\t", p.Row, p.Site, p.Username, p.Password)
+		}
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(listCmd)
+}
