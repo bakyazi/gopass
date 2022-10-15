d71401b4856ebc8e0d830206d50ded7413d2a7c5
Berkay Akyazi <berkay.akyazi@picussecurity.com>
2022-10-15 22:01:58 +0300

    delete command

---
+++ cmd/delete.go
@@ -0,0 +1,47 @@
+package cmd
+
+import (
+	"fmt"
+	"github.com/bakyazi/gopass/sheetsapi"
+	"github.com/spf13/cobra"
+)
+
+var deleteCmd = &cobra.Command{
+	Use: "delete",
+	Run: func(cmd *cobra.Command, args []string) {
+
+		site, err := cmd.Flags().GetString("site")
+		if err != nil {
+			fmt.Println("Cannot parse 'site' flag", err.Error())
+		}
+
+		username, err := cmd.Flags().GetString("username")
+		if err != nil {
+			fmt.Println("Cannot parse 'username' flag", err.Error())
+		}
+
+		db, err := sheetsapi.NewPasswordDB("credentials.local.json", "1u8W_mNDPl8xRsl2YPrqk46I2oOZ_NDnZUpFThRGMo5A")
+		if err != nil {
+			panic(err)
+		}
+		err = db.DeletePassword(site, username)
+		if err != nil {
+			fmt.Println("Failed to create and save passwords", err.Error())
+			return
+		}
+
+		fmt.Println("Success!")
+
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(deleteCmd)
+
+	deleteCmd.Flags().String("site", "", "Enter website")
+	_ = deleteCmd.MarkFlagRequired("site")
+
+	deleteCmd.Flags().String("username", "", "Enter username")
+	_ = deleteCmd.MarkFlagRequired("username")
+
+}
